be2bddcacec1b2b0f7251a42ea41e6b629e61021
Your Name <you@example.com>
2022-03-12 18:06:35 -0600

    gstmanager: Add the GstManager test skeleton

---
+++ tests/test_gstmanager.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import unittest
+
+
+class GstManagerTests(unittest.TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_start(self):
+        pass
+
+    def test_stop(self):
+        pass
